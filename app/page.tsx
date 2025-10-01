712f5b7838621263fa97a87b7a7f8f2046638e78
Ayush Tiwari <ayush0511tiwari@gmail.com>
2025-10-01 18:25:54 +0530

    project initilaisation done (set-up)

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <h1 className="text-5xl font-bold text-white">
+        🚀 Tailwind is Working!
+      </h1>
+    </main>
+  );
+}
